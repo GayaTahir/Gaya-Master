d650e4ac17367ff11c645cc037714f75953a7e32
Quantum Nexus <gayatahir09@gmail.com>
2025-10-07 22:37:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Gaya-Master/', // <— your repo name, case-sensitive
   plugins: [react()],
 })
